53e63aa14c4e43e0bb8873cfb595e7f6b6fae590
Jeff Coffler <REDMOND\jeffcof>
2015-03-23 14:42:37 -0700

    SCX Mega-bundle support

---
+++ installer/oss-kits/apache-oss-test.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+#
+# Note: FindApacheConfigFile() is found in apache/installer/conf/apache_config.sh!
+#       If this function is modified, modify in that file as well!
+#
+
+FindApacheConfigFile()
+{
+    if [ -z "${APACHE_CONF}" -o -z "${CONF_STYLE}" ]; then
+        # Source code installation w/default directories
+        APACHE_CONF=/usr/local/apache2/conf/httpd.conf
+        CONF_STYLE=f
+        [ -e "${APACHE_CONF}" ] && return
+
+        # Favor the conf.d-style directories
+        APACHE_CONF=/etc/httpd/conf.d
+        CONF_STYLE=d
+        [ -d "${APACHE_CONF}" ] && return
+
+        # Favor the conf.d-style directories
+        APACHE_CONF=/etc/apache2/conf.d
+        CONF_STYLE=d
+        [ -d "${APACHE_CONF}" ] && return
+
+        # Redhat-type installation
+        APACHE_CONF=/etc/httpd/conf/httpd.conf
+        CONF_STYLE=f
+        [ -e "${APACHE_CONF}" ] && return
+
+        # SuSE-type installation
+        APACHE_CONF=/etc/apache2/default-server.conf
+        CONF_STYLE=f
+        [ -e "${APACHE_CONF}" ] && return
+
+        # Ubuntu-type installation
+        APACHE_CONF=/etc/apache2/apache2.conf
+        CONF_STYLE=f
+        [ -e "${APACHE_CONF}" ] && return
+
+        # Unable to find it, so indicate so
+        APACHE_CONF=
+        CONF_STYLE=
+    fi
+}
+
+echo "Checking of Apache is installed ..."
+
+# Find anything?
+FindApacheConfigFile
+if [ -z "${APACHE_CONF}" -o -z "${CONF_STYLE}" ]; then
+    echo "  Apache not found, will not install"
+    exit 1
+fi
+
+echo "  Apache found, Apache agent will be installed"
+exit 0
---
+++ installer/oss-kits/mysql-oss-test.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+
+#
+# Note: FindClientLibrary() is found in mysql/installer/datafiles/linux.data!
+#       If this function is modified, modify in that file as well!
+#
+
+FindClientLibrary()
+{
+    MYSQL_NAME=libmysqlclient.so
+    CLIENT_LIBRARY=
+
+    # We use a variety of means to find libmysqlclient.so:
+    #
+    # 1. Via ldd of 'mysql' client program (may not be installed, or may link statically to MySQL)
+    # 2. Via ldconfig -p (doesn't work properly on all systems)
+    # 3. Search a list of likely directories
+    #
+    # If, after all that, we don't find it, then CLIENT_LIBRARY is empty, signifying a failure
+
+    MYSQL_PATH=`which mysql`
+    [ -n "${MYSQL_PATH}" ] && CLIENT_LIBRARY=`ldd ${MYSQL_PATH} | grep ${MYSQL_NAME} | awk '{print $3}'`
+
+    # Be aware that /sbin/ldconfig may return multiple hits; if so, we'll use the first one
+    [ -z "${CLIENT_LIBRARY}" ] && CLIENT_LIBRARY=`/sbin/ldconfig -p | grep ${MYSQL_NAME} | head -1 | awk '{print $NF}'`
+
+    if [ -z "${CLIENT_LIBRARY}" ]; then
+        SEARCH_LIST_64="/usr/lib64 /usr/lib64/mysql /lib64 /lib64/mysql /usr/lib/x86_64-linux-gnu"
+        SEARCH_LIST_32="/usr/lib /usr/lib/mysql /lib /lib/mysql /usr/lib/*86-linux-gnu /usr/local/mysql/lib"
+        # Fix for Debian systems - packages installed in /opt, library built into mysql cli program statically
+        SEARCH_LIST_32="$SEARCH_LIST_32 /opt/mysql/*/lib"
+
+        if [ `uname -m` = "x86_64" ]; then
+            SEARCH_LIST="${SEARCH_LIST_64} ${SEARCH_LIST_32}"
+        else
+            SEARCH_LIST="${SEARCH_LIST_32}"
+        fi
+
+        # echo "Searching for ${MYSQL_NAME} in: ${SEARCH_LIST}"
+        for i in ${SEARCH_LIST}; do
+            # echo "  Checking $i ..."
+            if [ -e $i/${MYSQL_NAME} ]; then
+                CLIENT_LIBRARY=$i/${MYSQL_NAME}
+                break
+            fi
+        done
+    fi
+}
+
+echo "Checking of MySQL is installed ..."
+
+# Find anything?
+FindClientLibrary
+if [ -z "${CLIENT_LIBRARY}" ]; then
+    echo "  MySQL not found, will not install"
+    exit 1
+fi
+
+echo "  MySQL found, MySQL agent will be installed"
+exit 0
