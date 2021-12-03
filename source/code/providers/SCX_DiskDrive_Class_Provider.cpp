8d70f4554b5cc0d0f18529e236b0bcf71999344b
dishantcr7 <64056848+dishantcr7@users.noreply.github.com>
2021-12-03 22:23:24 +0530

    Update SCX_DiskDrive_Class_Provider.cpp
    Changing char** initialization to avoid error: invalid conversion from 'char' to 'char*' [-fpermissive]

--- source/code/providers/SCX_DiskDrive_Class_Provider.cpp
+++ source/code/providers/SCX_DiskDrive_Class_Provider.cpp
@@ -196,8 +196,10 @@ void SCX_DiskDrive_Class_Provider::EnumerateInstances(
         size_t instancePos=(size_t)-1;
 
         if(filter) {
-            char* exprStr[QLENGTH]={'\0'};
-            char* qtypeStr[QLENGTH]={'\0'};
+            //char* exprStr[QLENGTH]={'\0'};
+	    char ** exprStr = new char *[QLENGTH];
+            //char* qtypeStr[QLENGTH]={'\0'};
+	    char ** qtypeStr = new char *[QLENGTH];
 
             const MI_Char** expr=(const MI_Char**)&exprStr;
             const MI_Char** qtype=(const MI_Char**)&qtypeStr;
